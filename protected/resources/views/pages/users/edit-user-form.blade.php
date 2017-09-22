1a188d867fd968ad32d84fc2da7a88a07467aa2d
Otoman <otomang@hotmail.com>
2017-09-22 14:33:52 -0400

    Add Datatables config

--- protected/resources/views/pages/users/edit-user-form.blade.php
+++ protected/resources/views/pages/users/edit-user-form.blade.php
@@ -3,7 +3,7 @@
 
           <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
            <label for="first_name">First Name:</label>
-           <input type="text" class="form-control" name="first_name" id="first_name" value="{{ old('first_name') }}" required autofocus>
+           <input type="text" class="form-control" name="first_name" id="first_name" value="{{ $user->first_name }}" required autofocus>
             @if ($errors->has('first_name'))
                 <span class="help-block">
                     <strong>{{ $errors->first('first_name') }}</strong>
@@ -12,7 +12,7 @@
          </div>
          <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
            <label for="last_name">Last Name:</label>
-           <input type="text" class="form-control" name="last_name" id="last_name" value="{{ old('last_name') }}" required autofocus>
+           <input type="text" class="form-control" name="last_name" id="last_name" value="{{ $user->last_name }}" required autofocus>
             @if ($errors->has('last_name'))
                 <span class="help-block">
                     <strong>{{ $errors->first('last_name') }}</strong>
@@ -21,24 +21,12 @@
          </div>
         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
          <label for="email">E-Mail Address</label>
-             <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+             <input id="email" type="email" class="form-control" name="email" value="{{ $user->email }}" required>
              @if ($errors->has('email'))
                  <span class="help-block">
                      <strong>{{ $errors->first('email') }}</strong>
                  </span>
              @endif
        </div>
-       <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-         <label for="password" >Password</label>
-            <input id="password" type="password" class="form-control" name="password" required>
-             @if ($errors->has('password'))
-                 <span class="help-block">
-                     <strong>{{ $errors->first('password') }}</strong>
-                 </span>
-             @endif
-     </div>
-     <div class="form-group">
-         <label for="password-confirm">Confirm Password</label>
-            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-     </div>
-     <button type="submit" class="btn btn-default">Submit</button>
+
+     <button type="submit" class="btn btn-default">Save Changes</button>
--- protected/resources/views/pages/users/edit.blade.php
+++ protected/resources/views/pages/users/edit.blade.php
@@ -7,11 +7,11 @@ Register
     <div class="row tile_count push-margin-down" id="app">
           <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Add New User</div>
+                <div class="panel-heading">Edit User</div>
                 <div class="panel-body">
                   <form class="" method="POST" action="{{ route('register') }}">
                       {{ csrf_field() }}
-                      @include('pages.users.add_new_user_form')
+                      @include('pages.users.edit-user-form')
                   </form>
                 </div>
             </div>
